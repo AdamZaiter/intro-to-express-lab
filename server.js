19842148e778a1590f36e2ccc54f833de273a080
Gellokd <adamzaiter23@gmail.com>
2024-07-08 11:08:25 -0500

    Got the Greetings to work

--- server.js
+++ server.js
@@ -4,9 +4,18 @@ const express = require('express');
 const app = express();
 // Creating blank instnace
 
-
+app.get('/', (req, res) => {
+    res.send('Hello, world!')
+})
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
 // App will listen for requests on port 3000
+
+app.get('/greetings/:username', (req, res) => {
+    const username = req.params.username;
+    res.send(`Hello there, ${username}`)
+}) 
+
+
